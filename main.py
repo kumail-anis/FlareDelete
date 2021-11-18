c668fe87f1f1c340b9cff42fe082d76624644447
Venus Parfait <70839827+vparfait@users.noreply.github.com>
2021-11-18 13:59:10 +1100

    Working version
    Utilises os, deletes all zip files from the specified folder and subdirectories

--- main.py
+++ main.py
@@ -1,24 +1,41 @@
 import os
 
 def main():
-    path = '/Users/venus.parfait/Downloads/sec-iam-clm-prd-ff2f-west-vm2_us/'
-    name = 'datadog-agent-'
+    path = "/Users/venus.parfait/Downloads"
+    name = "datadog-agent-"
+    extension = ".zip"
 
     #check whether path exists and is a directory
     if os.path.exists(path):
         if os.path.isdir(path):
             print("both all good")
 
+            #to-do first call to walk to get info; print the first file
+
             # iterating through the subfolders
-            #this doesn't work for downloads
             for root, dirs, files in os.walk(path):
                 for name in files:
-                    print(os.path.join(root, name))
+                    # file path
+                    file_path = os.path.join(root, name)
 
+                    # extracting the extension from the filename
+                    file_extension = os.path.splitext(file_path)[1]
+
+
+                    # checking the file_extension
+                    if extension == file_extension:
+                        # deleting the file
+                        if not os.remove(file_path):
+                            # success message
+                            print("file deleted successfully")
+                            
+                        else:
+                            # failure message
+                            print("Unable to delete the file")
         else:
-            print(f"{path} is not a directory")
+            print("is not a directory")
     else:
-        print(f"{path} does not exist")
+        print("does not exist")
 
 if __name__ == '__main__':
     main()
