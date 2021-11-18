4baa7e80455573093c827645774c0d1d103414d4
Venus Parfait <70839827+vparfait@users.noreply.github.com>
2021-11-18 11:54:06 +1100

    Update

--- main.py
+++ main.py
@@ -1,15 +1,24 @@
 import os
 
 def main():
-    path = '/Users/venus.parfait/Downloads'
+    path = '/Users/venus.parfait/Downloads/sec-iam-clm-prd-ff2f-west-vm2_us/'
     name = 'datadog-agent-'
 
     #check whether path exists and is a directory
     if os.path.exists(path):
         if os.path.isdir(path):
             print("both all good")
+
+            # iterating through the subfolders
+            #this doesn't work for downloads
+            for root, dirs, files in os.walk(path):
+                for name in files:
+                    print(os.path.join(root, name))
+
         else:
-            print("not valid")
+            print(f"{path} is not a directory")
+    else:
+        print(f"{path} does not exist")
 
 if __name__ == '__main__':
     main()
