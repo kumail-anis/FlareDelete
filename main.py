6d7fd358fbd7dfda82f6230c33477462fa7b2445
Venus Parfait <70839827+vparfait@users.noreply.github.com>
2021-11-19 12:00:49 +1100

    Add remove extracted flare function

--- main.py
+++ main.py
@@ -1,19 +1,28 @@
 import os
+from pathlib import Path
 
 def main():
-    #change path to Kumail's method
-    path = "/Users/venus.parfait/Documents/test/notaflare.zip"
+    #main will delete any .zip files in your downloads folder where datadog-agent is part of the path
+    #old method
+    #path = "/Users/venus.parfait/Documents/test/notaflare.zip"
     zipFlareName = "datadog-agent"
     extension = ".zip"
+    #get home directory
+    home_dir = str(Path.home())
+    search_path = str(Path.home() / "Documents/test/")
+
+    #testing
+    print(home_dir)
+    removeExtractedFlares()
 
     #check whether path exists and is a directory
-    if os.path.exists(path):
-        if os.path.isdir(path):
+    if os.path.exists(search_path):
+        if os.path.isdir(search_path):
             #print("both all good")
             #to-do first call to walk to get info; print the first file
 
             # iterating through the subfolders
-            for root, dirs, files in os.walk(path):
+            for root, dirs, files in os.walk(search_path):
                 for name in files:
                     # get the file path
                     file_path = os.path.join(root, name)
@@ -34,12 +43,36 @@ def main():
                                 # failure message
                                 print("Unable to delete the file")
                         else:
-                            print("File is not a flare, do nothing")
+                            print("File is not a flare")
         else:
-            print(path, "is not a directory")
+            print(f"{search_path} is not a directory")
     else:
-        print(path,"does not exist")
-        #cannot use print(f"{path} blah blah")
+        print(f"{search_path} does not exist")
+
+def removeExtractedFlares():
+    #lets set a static path until we get Kumail's section
+    #path is inside an extracted flare; the name is unimportant here
+    static_path = "/Users/venus.parfait/Downloads/saltabcc104_pat_csl_cloud_td_com 2/status.log"
+
+    #also set statically until Kumail's part
+    statusLogPresent = True
+    
+    #if directory contains a status log we know the parent directory is an extracted flare
+    if statusLogPresent:
+        #do work
+
+        #Split our path string based on the /'s 
+        #This will assume that the path of an extracted flare follows the format: /Users/<user>/Downloads/<flare_name>
+        splitPath = static_path.split("/")
+
+        print(f"{static_path} path before splitting")
+        print(f"{splitPath} path after splitting")
+        #returns a list of 6 strings remove the first and last entry
+
+        #drop the first and last entry
+        print(splitPath[1:5])
+        
+
 
 if __name__ == '__main__':
     main()
