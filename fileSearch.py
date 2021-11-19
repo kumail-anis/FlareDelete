3f3ca13c0a1e77f00d88866f36f7f65db1eb917a
kumail-anis <kumail.anis@datadoghq.com>
2021-11-19 16:14:47 +1100

    merged files into main.py

--- fileSearch.py
+++ fileSearch.py
@@ -1,57 +1,53 @@
-# File created from following resource: https://www.opentechguides.com/how-to/article/python/59/files-containing-text.html
 import os
 from pathlib import Path
 
-class fileSearch:
+def fileSearchPath():
 
-    def fileSearchPath():
+    # print(str(Path.home() / "Downloads"))
+    # print(os.path.dirnmae(__file__) / "testingProject")
+    # downloads_Directory = str(os.path.dirname(__file__))
+    # search_path = downloads_Directory + "/testingProject"
 
-        # print(str(Path.home() / "Downloads"))
-        # print(os.path.dirnmae(__file__) / "testingProject")
-        # downloads_Directory = str(os.path.dirname(__file__))
-        # search_path = downloads_Directory + "/testingProject"
+    search_path = str(Path.home() / "Downloads")
+    file_type = [".yaml", ".yml", ".csv"]
+    search_str = ["replicaCount:", "datadog:", "datadogAgents:", "podAnnotations:", "kind: DaemonSet", "kind: Deployment", "AWSTemplateFormatVersion:", "Average Custom Metrics / Hour", "init_config:", "Getting the status from the agent"]
 
-        search_path = str(Path.home() / "Downloads")
-        file_type = [".yaml", ".yml", ".csv"]
-        search_str = ["replicaCount:", "datadog:", "datadogAgents:", "podAnnotations:", "kind: DaemonSet", "kind: Deployment", "AWSTemplateFormatVersion:", "Average Custom Metrics / Hour", "init_config:"]
+    # Append a directory separator if not already present
+    if not (search_path.endswith("/") or search_path.endswith("\\") ): 
+            search_path = search_path + "/"
+                                                            
+    # If path does not exist, set search path to current directory
+    if not os.path.exists(search_path):
+        search_path =str(Path.home() / "Downloads")
 
-        # Append a directory separator if not already present
-        if not (search_path.endswith("/") or search_path.endswith("\\") ): 
-                search_path = search_path + "/"
-                                                                
-        # If path does not exist, set search path to current directory
-        if not os.path.exists(search_path):
-                search_path =str(Path.home() / "Downloads")
+    # Repeat for each file in the directory  
+    for fname in os.listdir(path=search_path):
+        
+        fileDirectory = search_path + fname
+        # Apply file type filter   
+        for fileType in file_type:
+            if fname.endswith(fileType):
 
-        # Repeat for each file in the directory  
-        for fname in os.listdir(path=search_path):
-            
-            fileDirectory = search_path + fname
-            # Apply file type filter   
-            print("test")
-            for fileType in file_type:
-                if fname.endswith(fileType):
+                # Open file for reading
+                fo = open(fileDirectory)
+                # Read the first line from the file
+                line = fo.readline()
 
-                    # Open file for reading
-                    fo = open(fileDirectory)
-                    # Read the first line from the file
-                    line = fo.readline()
+                # Loop until EOF
+                while line != '' :
+                        # Search for string in line
+                        for element in search_str:
+                            index = line.find(element)
+                            if ( index != -1) :
 
-                    # Loop until EOF
-                    while line != '' :
-                            # Search for string in line
-                            for element in search_str:
-                                index = line.find(element)
-                                if ( index != -1) :
+                                if(os.path.exists(search_path + fname)):
+                                    print(f"{fileDirectory} file deleted")
+                                    os.remove(search_path + fname)
 
-                                    if(os.path.exists(search_path + fname)):
-                                        print(fileDirectory + " file deleted")
-                                        os.remove(search_path + fname)
+                        # Read next line
+                        line = fo.readline()  
 
-                            # Read next line
-                            line = fo.readline()  
+                fo.close()
 
-                    fo.close()
-
-    if __name__ == '__main__':
-        fileSearchPath()
+if __name__ == '__main__':
+    fileSearchPath()
--- main.py
+++ main.py
@@ -1,87 +1,14 @@
-import os
-from pathlib import Path
-import shutil
+import fileSearch
+import zipDeletion
+import extractedFlareDeletion
 
 def main():
-    #This will delete any .zip files in your downloads folder where datadog-agent is part of the path
-    zipFlareName = "datadog-agent"
-    extension = ".zip"
-    search_path = str(Path.home() / "Downloads")
-
-    #removeExtractedFlares()
-
-    #check whether path exists and is a directory
-    if os.path.exists(search_path):
-        if os.path.isdir(search_path):
-            # iterating through the subfolders
-            for root, dirs, files in os.walk(search_path):
-                for name in files:
-                    # get the file path
-                    file_path = os.path.join(root, name)
-
-                    # extract the extension from the filename
-                    file_extension = os.path.splitext(file_path)[1]
-
-                    #Check if the file_extension is a zip
-                    if extension == file_extension:
-                        #Check that the file_path contains zipFlareName - could improve
-                        if zipFlareName in file_path:
-                        # Delete the file
-                            if not os.remove(file_path):
-                                # success message
-                                print("File deleted successfully")
-                                
-                            else:
-                                # failure message - possibly permissions issue
-                                print("Unable to delete the file")
-                        else:
-                            print("File is not a flare")
-        else:
-            print(f"{search_path} is not a directory")
-    else:
-        print(f"{search_path} does not exist")
-
-def removeExtractedFlares():
-    startPath = str(Path.home() / "Downloads")
-    file_name = ["status.log", "cluster-agent-status.log"]
-
-    #check if there is a slash at the end of path, if not add one
-    if not (startPath.endswith("/") or startPath.endswith("\\") ): 
-        startPath = startPath + "/"
-
-    for root, dirs, files in os.walk(startPath):
-        #this is each directory
-        for name in dirs:
-
-            targetPath = startPath + name + "/"
-
-            #Check if current path exists
-            if os.path.exists(targetPath):
-                #fname is each filename within the folder (startPath)
-                for fname in os.listdir(targetPath):
-                    fileDirectory = targetPath + fname
-                    for fileType in file_name:
-                            if fileType in fname:
-                                #file directory is the full path
-                                #CHECK THIS LATER
-                                if(os.path.exists(fileDirectory)):                        
-                                    #See if we can use targetPath instead
-                                    # Split our path string based on the /'s 
-                                    #This will assume that the path of an extracted flare follows the format: /Users/<user>/Downloads/<flare_name>
-                                    splitPath = fileDirectory.split("/")
-
-                                    #Drop the first and last entry
-                                    slicedPath = splitPath[1:5]
-
-                                    #Now we remake the path and remove the file
-                                    pathToRemove = "/" + slicedPath[0] + "/" + slicedPath[1] + "/" + slicedPath[2] + "/" + slicedPath[3]
-                                    shutil.rmtree(pathToRemove)
-                                    print("folder removed")
-            else:
-                print(f"{targetPath} does not exist")
-
-        
-
+    try:
+        zipDeletion.zipDeletion()
+        extractedFlareDeletion.removeExtractedFlares()
+        fileSearch.fileSearchPath()
+    except SyntaxError:
+        print("file was not found")
 
 if __name__ == '__main__':
     main()
